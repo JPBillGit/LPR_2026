119ae906248d8b8f2ecfca526c55edab530f66d2
JPBillGit <jpbillgithub@gmail.com>
2026-03-17 14:04:46 -0300

    Add files via upload

---
+++ Aula4/RPG/RPG/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Escolha uma classe :>");
+        Console.WriteLine("1- Guerreiro");
+        Console.WriteLine("2- Mago");
+        Console.WriteLine("3- Arqueiro");
+
+        int escolha = int.Parse(Console.ReadLine());
+
+        switch (escolha)
+        {
+            case 1:
+                Console.WriteLine("Classe: Guerreiro");
+                Console.WriteLine("Habilidades:");
+                Console.WriteLine("- Golpe Poderoso");
+                Console.WriteLine("- Defesa Total");
+                break;
+
+            case 2:
+                Console.WriteLine("Classe: Mago");
+                Console.WriteLine("Habilidades:");
+                Console.WriteLine("- Bola de Fogo");
+                Console.WriteLine("- Teletransporte");
+                break;
+
+            case 3:
+                Console.WriteLine("Classe: Arqueiro");
+                Console.WriteLine("Habilidades:");
+                Console.WriteLine("- Flecha Precisa");
+                Console.WriteLine("- Chuva de Flechas");
+                break;
+
+            default:
+                Console.WriteLine("Opção inválida!");
+                break;
+        }
+    }
+}
