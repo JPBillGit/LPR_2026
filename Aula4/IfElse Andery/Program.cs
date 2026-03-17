d2e59b8004bde6b4bfb23515381f9173e36e13fb
JPBillGit <jpbillgithub@gmail.com>
2026-03-17 13:57:23 -0300

    Add files via upload

---
+++ Aula4/IfElse Andery/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um númer inteiro: ");
+
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"{numero} é um número par :3");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} é um número ímpar >:(");
+        }
+        }
+    }
