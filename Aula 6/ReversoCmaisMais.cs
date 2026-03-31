49ecc333529a372650a8c53183e1b63119deaed7
JPBillGit <jpbillgithub@gmail.com>
2026-03-31 13:56:47 -0300

    Add files via upload

---
+++ Aula 6/ReversoCmaisMais.cs
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+long long inverterNumero(int n) {
+    long long reverso = 0;
+    while (n != 0) {
+        int digito = n % 10;
+        reverso = reverso * 10 + digito;
+        n /= 10;
+    }
+    return reverso;
+}
+
+int main() {
+    int numero = 12345;
+    std::cout << "Original: " << numero << std::endl;
+    std::cout << "Reverso: " << inverterNumero(numero) << std::endl;
+    
+    return 0;
+}
